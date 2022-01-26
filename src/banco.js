933e6150e5e38bd8ce6b07030c21fa07641bc49f
Tiago Bezerra dos Santos <bsantostiago@gmail.com>
2022-01-26 10:31:09 -0300

    Update dos dados da api

--- src/banco.js
+++ src/banco.js
@@ -1,17 +1,24 @@
 import mysql from 'mysql2';
 
 const conexao = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'escola'
+    /* LOCAL */
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'escola'
+
+    /* REMOTO */
+    host: 'db4free.net',
+    user: 'admin_api_tiago',
+    password: 'Senac*123',
+    database: 'escola_tiago'
 });
 
 conexao.connect(erro => {
     if(erro){
         console.error(`Erro ao conectar: ${erro.message}`);
     } else {
-        console.log(`Banco de dados conectado com sucesso!`);
+        console.log(`Banco conectado em: ${conexao.config.host}`);
     }
 });
 
